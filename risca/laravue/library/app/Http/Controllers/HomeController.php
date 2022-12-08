9b40131b662bb4c326256e92cd5d36197c09d1ee
rivitrei <riscanovita02@gmail.com>
2022-12-08 17:57:04 +0700

    task eloquent relationship model

--- risca/laravue/library/app/Http/Controllers/HomeController.php
+++ risca/laravue/library/app/Http/Controllers/HomeController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
+use App\Models\Member;
+use App\Models\Publisher;
+use App\Models\Author;
+use App\Models\Catalog;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +28,13 @@ class HomeController extends Controller
      */
     public function index()
     {
+        // $members = Member::with('user')->get();
+        // $books = Book::with('publisher')->get();
+        // $publishers = Publisher::with('books')->get();
+         // $authors = Author::with('books')->get();
+        $catalogs = Catalog::with('books')->get();
+
+        return$catalogs;
         return view('home');
     }
 }
