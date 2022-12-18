e48a346f6e7c9985513279ad4b04c32cf579489d
Lilyan <lilyan0800@gmail.com>
2022-12-18 20:39:34 +0700

    CRUD (Without Vue js)

--- lilyan/Laravel Vue/library/app/Http/Controllers/CatalogController.php
+++ lilyan/Laravel Vue/library/app/Http/Controllers/CatalogController.php
@@ -27,7 +27,7 @@ class CatalogController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.catalog.create');
     }
 
     /**
@@ -38,7 +38,19 @@ class CatalogController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'name' => ['required'],
+        ]);
+
+        // Cara pertama
+        // $catalog = new Catalog;
+        // $catalog->name = $request->name;
+        // $catalog->save();
+
+        // Cara kedua (ada tambahan di modelnya)
+        Catalog::create($request->all());
+
+        return redirect('catalogs');
     }
 
     /**
@@ -60,7 +72,8 @@ class CatalogController extends Controller
      */
     public function edit(Catalog $catalog)
     {
-        //
+        // return $catalog;
+        return view('admin.catalog.edit', compact('catalog'));
     }
 
     /**
@@ -72,7 +85,13 @@ class CatalogController extends Controller
      */
     public function update(Request $request, Catalog $catalog)
     {
-        //
+        $this->validate($request, [
+            'name' => ['required'],
+        ]);
+
+        $catalog->update($request->all());
+
+        return redirect('catalogs');
     }
 
     /**
@@ -83,6 +102,8 @@ class CatalogController extends Controller
      */
     public function destroy(Catalog $catalog)
     {
-        //
+        $catalog->delete();
+
+        return redirect('catalogs');
     }
 }
