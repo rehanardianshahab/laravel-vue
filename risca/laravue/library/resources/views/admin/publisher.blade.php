4953a3bdec44a16376e512b0651131f821e25d36
rivitrei <riscanovita02@gmail.com>
2022-12-19 18:48:28 +0700

    task crud with vue js part 2

--- risca/laravue/library/resources/views/admin/publisher.blade.php
+++ risca/laravue/library/resources/views/admin/publisher.blade.php
@@ -2,7 +2,10 @@
 @section('header', 'Publisher')
 
 @section('css')
-
+<!-- Datatables -->
+<link rel="stylesheet" href="{{ asset('assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
 @endsection
 
 @section('content')
@@ -14,8 +17,8 @@
 					<a href="#" @click="addData()" class="btn btn-sm btn-primary pull-right">Create New Publisher</a>
 				</div>
 			 
-				<div class="card-body p-0">
-					<table class="table table-bordered">
+				<div class="card-body">
+					<table id="datatable" class="table table-bordered table-striped">
 						<thead>
 							<tr>
 								<th>No</th>
@@ -29,12 +32,12 @@
 						<tbody>
 							@foreach($publishers as $key => $publisher)
 							<tr>
-								<td>{{ $key+1 }}</td>
+								<td class="text-center">{{ $key+1 }}</td>
 								<td>{{ $publisher->name }}</td>
 								<td>{{ $publisher->email }}</td>
-								<td>{{ $publisher->phone_number }}</td>
+								<td class="text-center">{{ $publisher->phone_number }}</td>
 								<td>{{ $publisher->address }}</td>
-								<td class="text-right">
+								<td class="text-center">
 									<a href="#" @click="editData({{ $publisher }})" class="btn btn-warning btn-sm">Edit</a>
 									<a href="#" @click="deleteData({{ $publisher->id }})" class="btn btn-danger btn-sm">Delete</a>
 								</td>
@@ -93,6 +96,26 @@
 @endsection
 
 @section('js')
+<!-- DataTables & Plugins -->
+<script src="{{ asset('assets/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/jszip/jszip.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/pdfmake/pdfmake.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/pdfmake/vfs_fonts.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
+<!-- js memanggil plugin datatables -->
+<script type="text/javascript">
+	$(function () {
+    $("#datatable").DataTable();
+  });
+</script>
+<!-- CRUD Vue Js -->
 <script type="text/javascript">
  	var controller = new Vue({
  		el: '#controller',
