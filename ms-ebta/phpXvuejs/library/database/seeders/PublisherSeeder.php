7549b2203b95cb09e58cbef411614a4803b32e33
MasEBTA <ebta.sb@gmail.com>
2022-12-22 10:39:56 +0700

    membuat data seeder publisher

--- ms-ebta/phpXvuejs/library/database/seeders/PublisherSeeder.php
+++ ms-ebta/phpXvuejs/library/database/seeders/PublisherSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Publisher;
+use Faker\Factory as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +16,22 @@ class PublisherSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // memanggil faker
+        $faker = faker::create();
+        // script looping
+        for ($i=0; $i < 20; $i++) { 
+            $buku = new Book;
+
+            $buku->isbn = 'ISBN-' . $faker->randomNumber(3) . '-' . $faker->randomNumber(3) . '-' . $faker->randomNumber(3) . '-' . $faker->randomNumber(3) . '-' . $faker->randomNumber(1);
+            $buku->title = $faker->name;
+            $buku->year = rand(1990,2021);
+            $buku->publisher_id = rand(1, 20);
+            $buku->author_id = rand(1, 20);
+            $buku->catalog_id = rand(1, 20);
+            $buku->qty = rand(1, 40);
+            $buku->price = rand(10000, 50000);
+
+            $buku->save();
+        }
     }
 }
