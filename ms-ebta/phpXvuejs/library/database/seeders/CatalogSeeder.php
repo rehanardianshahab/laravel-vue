3590b14e74f75e07665ca408ae2cd529be3f7298
MasEBTA <ebta.sb@gmail.com>
2022-12-22 10:38:00 +0700

    membuat seeder data catalog

--- ms-ebta/phpXvuejs/library/database/seeders/CatalogSeeder.php
+++ ms-ebta/phpXvuejs/library/database/seeders/CatalogSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Catalog;
+use Faker\Factory as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +16,15 @@ class CatalogSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // memanggil faker
+        $faker = faker::create();
+        // script looping
+        for ($i=0; $i < 20; $i++) { 
+            $buku = new Book;
+
+            $buku->name = $faker->name;
+
+            $buku->save();
+        }
     }
 }
