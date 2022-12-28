24f8dce1bdf79f23bf2f56b93ec15f02af591056
MasEBTA <ebta.sb@gmail.com>
2022-12-28 22:25:04 +0700

    membuat file seeder Transaction

--- ms-ebta/phpXvuejs/library/app/Models/Transaction.php
+++ ms-ebta/phpXvuejs/library/app/Models/Transaction.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     use HasFactory;
+    
+    // relasi database
+    public function Member()
+    {
+        return $this->belongsTo('App\Models\User', 'member_id');
+    }
 }
