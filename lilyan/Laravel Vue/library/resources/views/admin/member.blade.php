454245dff4a4f757d24e733be61c0ed2310ef642
Lilyan <lilyan0800@gmail.com>
2023-01-01 13:07:04 +0700

    Task Helpers

--- lilyan/Laravel Vue/library/resources/views/admin/member.blade.php
+++ lilyan/Laravel Vue/library/resources/views/admin/member.blade.php
@@ -27,6 +27,7 @@
                       <th class="text-center">Phone Number</th>
                       <th class="text-center">Address</th>
                       <th class="text-center">Email</th>
+                      <th class="text-center">Created At</th>
                       <th class="text-right">Action</th>
                     </tr>
                   </thead>
@@ -117,6 +118,7 @@
     {data: 'phone_number', class: 'text-center', orderable: true},
     {data: 'address', class: 'text-center', orderable: true},
     {data: 'email', class: 'text-center', orderable: true},
+    {data: 'date', class: 'text-center', orderable: true},
     {render: function (index, row, data, meta) {
       return `
       <a href="#" class="btn btn-warning btn-sm" onclick="controller.editData(event,
