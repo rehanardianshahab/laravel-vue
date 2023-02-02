05759e46c584091f4b1ca22cc245850013a4a439
MasEBTA <ebta.sb@gmail.com>
2023-02-02 17:56:40 +0700

    membuat method api() di cotroller author

--- ms-ebta/phpXvuejs/library/app/Http/Controllers/AuthorController.php
+++ ms-ebta/phpXvuejs/library/app/Http/Controllers/AuthorController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 
 class AuthorController extends Controller
 {
+    public function api()
+    {
+        # code...
+    }
     /**
      * Display a listing of the resource.
      *
