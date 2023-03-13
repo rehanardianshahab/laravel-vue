1d7f6fa06a84dbf910ee7f5274a06a36a1695bc4
MasEBTA <ebta.sb@gmail.com>
2023-03-13 23:24:49 +0700

    membuat label price

--- ms-ebta/cashiere/resources/views/print/product.blade.php
+++ ms-ebta/cashiere/resources/views/print/product.blade.php
@@ -9,6 +9,17 @@
         .text-center {
             text-align: center;
         }
+        .barcode-cover>div {
+            text-align: center;
+            display: inline-block;
+        }
+        .barcode-cover {
+            margin-bottom: 1rem;
+        }
+        .barcode-cover>p {
+            font-size: 0.6rem;
+            color: rgb(73, 79, 85);
+        }
     </style>
 </head>
 <body>
@@ -17,9 +28,15 @@
             @if ( $key == 0 )
             <tr>
             @endif
-                <td class="text-center" style="border: 1px solid;">
-                    <p>{{ $item->name }} - {{ money_format($item->selling_price, '.', 'Rp ', ',-') }}</p>
-                    {!! DNS1D::getBarcodeSVG($item->code, 'C39', 1,40, 'red') !!}
+                <td style="border: 1px solid;">
+                    {{ env('APP_NAME') }}
+                    <p class="text-center" >{{ $item->name }} - {{ money_format($item->selling_price, '.', 'Rp ', ',-') }}</p>
+                    {{-- {!! DNS1D::getBarcodeSVG($item->code, 'C39', 1,40, 'red') !!} --}}
+                    <div class="barcode-cover text-center">
+                        {!!  DNS1D::getBarcodeHTML($item->code, 'C39', 1,40) !!}
+                        <p>{{ $item->code }}</p>
+                    </div>
+                    
                     {{-- <img src="data:image/png;base64, {!! DNS2D::getBarcodePNG('4', 'PDF417') !!}" alt="barcode" style="min-height: 2rem; min-width:3rem;"/> --}}
                 </td>
             @if ( ($key+1) %3 == 0 )
